b760ed2b4eba83048375ee65233a9827fe3c6257
sarodri <sandra.ropero1987@gmail.com>
2023-05-09 09:15:26 +0200

    Footer

--- footer.php
+++ footer.php
@@ -1,15 +1,18 @@
 <footer>
     <div class="container">
      <!-- <?php dynamic_sidebar( 'footer' )?> -->
-        <div class="row d-flex">
-            <div class="col-9 d-flex copyright">
-                <span class="dashicons dashicons-pets"></span>
-                <p>Peluquería canina y spa Pluto © 2023 Todos los derechos reservados.</p>
-            </div>
-            <div class="col-3 links">
+        <div class="logo">
+                <span class="dashicons dashicons-pets"> </span>
+                <p>Peluquería canina y spa Pluto</p>
+                
+             <div class="links">
                 <a href="https://www.instagram.com/peluqueriacaninayspapluto/" target="_blank" alt="instagram" ><span class="dashicons dashicons-instagram"></span></a>
                 <a href="https://www.facebook.com/peluqueriacaninayspapluto" target="_blank" alt="facebook"><span class="dashicons dashicons-facebook"></span></a>
                 <a href="https://www.google.com/maps/place/C.+Virgen+de+la+Salud,+1,+14004+C%C3%B3rdoba/@37.8779355,-4.7916199,17z/data=!3m1!4b1!4m5!3m4!1s0xd6d209f64c3ac4b:0xbd1e1196f27c0823!8m2!3d37.8779355!4d-4.7894312" target="_blank" alt="gmaps"><span class="dashicons dashicons-location"></span> </a>
+            </div>
+            <div class="legal">
+                <p>© 2023 Todos los derechos reservados.</p>
+                <a target="_blank" href="https://github.com/sarodri">Website creada por sarodri.</a>
             </div>
         </div>
     </div>
