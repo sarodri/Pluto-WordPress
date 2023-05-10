2d1c1501ed969f1c7cb7f5c17e275daf67611cb6
sarodri <sandra.ropero1987@gmail.com>
2023-05-10 13:47:06 +0200

    Carrusel

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <main class="container my-5">
-    <div class="my-5 nosotros d-flex">
+    <div class="nosotros d-flex">
         <div class="nosotros-desc">
             <h2>Sobre nosotros</h2>
             <p>
@@ -15,16 +15,32 @@
              
         </div>
         <div class="nosotros-img">
-        <img src="<?php echo get_template_directory_uri()?>/assets/img/fachada.jpeg" alt="sobre-nosotros">
+            <img src="<?php echo get_template_directory_uri()?>/assets/img/fachada.jpeg" alt="sobre-nosotros">
         </div>
 
     </div>
-
-   
-    <div class="tratamientos">
-        <h2>Nuestros servicios</h2>
+    <div class="tratamientos d-flex">
+        <div class="tratamientos-carrusel">
+            <?php masterslider(1); ?>
+        </div>
+        <div class="tratamientos-desc">
+            <h2>Nuestros servicios</h2>
+            <ul>
+                <li>Máquina</li>
+                <li>Tijera</li>
+                <li>Stripping</li>
+                <li>Deslanado</li>
+                <li>Baños terapeúticos</li>
+                <li>Ozonoterapia</li>
+                <li>Spa</li>
+                <li>Arcilloterapia</li>
+                <li>Y más...</li>
+            </ul>
+        </div>
+     
+        
     
-        <div class="row my-3" id="tratamientos">
+        <!-- <div class="row my-3" id="tratamientos">
         <?php 
         $args = array(
             'post_type' => 'tratamiento',
@@ -48,7 +64,7 @@
                 </div>
         <?php } 
         } ?>
-        </div>
+        </div> -->
     </div>
 </main>
 
--- single.php
+++ single.php
@@ -1,22 +1,18 @@
 <?php get_header( ); ?>
 
 <main class='container my-3'>
-    <?php if(have_posts(  )){
-        while(have_posts(  )){
-            the_post(  );
-        ?>
-            <h1 class='my-4'><?php the_title( ); ?></h1>
+<?php if(have_posts(  )){
+                while(have_posts(  )){
+                the_post(  ); ?>
+                <h1 class='my-4'><?php the_title( ); ?></h1>
             <div class="row">
                 <div class="col-4">
                     <?php the_post_thumbnail( 'large'); ?>
                 </div>
                 <div class="col-8">
                     <?php the_content(); ?>
-                </div>
-            </div>
-        <?php
-        }
-    }?>
+                <?php   }
+                }?>
 </main>
 
 <?php get_footer( ); ?>
