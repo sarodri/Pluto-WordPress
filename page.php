00853a40aad237b88f229b16e8e3f159c63beb7e
sarodri <sandra.ropero1987@gmail.com>
2023-05-25 13:20:36 +0200

    Editing page

--- page.php
+++ page.php
@@ -4,10 +4,13 @@
     <?php if (have_posts()) {
         while(have_posts(  )){
             the_post(); ?>
-            <h1 class="my-5"><?php the_title(); ?></h1>
+            <h1 class="my-5 contacta"><?php the_title(); ?></h1>
             <?php the_content(); ?>
         <?php }
     }?>
+    <div class="contacto">
+        <p>O si lo prefieres, puedes llamar al télefono 662 27 96 89. Si no respondo es que estoy ocupada con algún perrito.</p>
+    </div>
 </main>
 
 <?php get_footer(); ?>
